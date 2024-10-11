73ba42583b520f34844c1146799cce77679b3435
ShaderFax <westleyhowe@hotmail.com>
2024-10-11 11:44:36 -0600

    in god I trust my design to look half decent

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,30 @@ function App() {
   return (
     <>
       <header>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <div className='header-container'>
           <h1>Westley Howe</h1>
           <div className='space-between'></div>
-          <button>Software</button>
+          <button>Projects</button>
           <button>Certificates</button>
-          <button>Contact</button>
+          <button></button>
         </div>
       </header>
       <body>
-        b
+        
       </body>
+      <footer>
+        <div className='footer-container'>
+          <div className='contact-container'>
+            <ol className='contact'>
+              <li><a>westleyhowe@hotmail.com</a></li>
+              <li>(639)999-2720</li>
+            </ol>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
 
-export default App
+export default App;
